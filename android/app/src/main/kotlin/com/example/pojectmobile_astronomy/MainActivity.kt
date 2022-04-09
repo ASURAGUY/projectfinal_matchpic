5c7511854807f40b4dd6ce2bbc4b019253ac4019
Nanthachanok Y <yodsing_n@silpakorn.edu>
2022-04-09 07:33:12 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pojectmobile_astronomy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pojectmobile_astronomy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,87 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flutter/cupertino.dart';
+
+enum Level { Hard, Medium, Easy }
+
+List<String> fillSourceArray() {
+  return [
+    'assets/images/dino.png',
+    'assets/images/dino.png',
+    'assets/images/wolf.png',
+    'assets/images/wolf.png',
+    'assets/images/peacock.png',
+    'assets/images/peacock.png',
+    'assets/images/whale.png',
+    'assets/images/whale.png',
+    'assets/images/octo.png',
+    'assets/images/octo.png',
+    'assets/images/fish.png',
+    'assets/images/fish.png',
+    'assets/images/frog.png',
+    'assets/images/frog.png',
+    'assets/images/seahorse.png',
+    'assets/images/seahorse.png',
+    'assets/images/girraf.png',
+    'assets/images/girraf.png',
+  ];
+}
+
+List getSourceArray(
+    Level level,
+    ) {
+  List<String> levelAndKindList = new List<String>();
+  List sourceArray = fillSourceArray();
+  if (level == Level.Hard) {
+    sourceArray.forEach((element) {
+      levelAndKindList.add(element);
+    });
+  } else if (level == Level.Medium) {
+    for (int i = 0; i < 12; i++) {
+      levelAndKindList.add(sourceArray[i]);
+    }
+  } else if (level == Level.Easy) {
+    for (int i = 0; i < 6; i++) {
+      levelAndKindList.add(sourceArray[i]);
+    }
+  }
+
+  levelAndKindList.shuffle();
+  return levelAndKindList;
+}
+
+List<bool> getInitialItemState(Level level) {
+  List<bool> initialItemState = new List<bool>();
+  if (level == Level.Hard) {
+    for (int i = 0; i < 18; i++) {
+      initialItemState.add(true);
+    }
+  } else if (level == Level.Medium) {
+    for (int i = 0; i < 12; i++) {
+      initialItemState.add(true);
+    }
+  } else if (level == Level.Easy) {
+    for (int i = 0; i < 6; i++) {
+      initialItemState.add(true);
+    }
+  }
+  return initialItemState;
+}
+
+List<GlobalKey<FlipCardState>> getCardStateKeys(Level level) {
+  List<GlobalKey<FlipCardState>> cardStateKeys =
+  new List<GlobalKey<FlipCardState>>();
+  if (level == Level.Hard) {
+    for (int i = 0; i < 18; i++) {
+      cardStateKeys.add(GlobalKey<FlipCardState>());
+    }
+  } else if (level == Level.Medium) {
+    for (int i = 0; i < 12; i++) {
+      cardStateKeys.add(GlobalKey<FlipCardState>());
+    }
+  } else if (level == Level.Easy) {
+    for (int i = 0; i < 6; i++) {
+      cardStateKeys.add(GlobalKey<FlipCardState>());
+    }
+  }
+  return cardStateKeys;
+}
